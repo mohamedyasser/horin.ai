12d0675eec03352b4bde1f977d266a6694bddff7
mohamedyasser <yasoesr@gmail.com>
2025-12-16 18:07:20 +0200

    feat: add localization for verification code error messages in Arabic and English

---
+++ lang/ar/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'verification_code_invalid' => 'رمز التحقق غير صالح أو منتهي الصلاحية.',
+];
---
+++ lang/en/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'verification_code_invalid' => 'The verification code is invalid or has expired.',
+];
