bff75d603e03b44179e9d0e78dc5ddf5f18c0bd9
mohamedyasser <yasoesr@gmail.com>
2025-12-17 10:42:52 +0200

    feat: remove invalid verification code messages and update phone sharing prompts for localization

--- lang/ar/messages.php
+++ lang/ar/messages.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'verification_code_invalid' => 'رمز التحقق غير صالح أو منتهي الصلاحية.',
     'telegram_share_phone_prompt' => 'يرجى مشاركة رقم هاتفك للتحقق من حسابك.',
     'share_phone_button' => 'مشاركة رقم الهاتف',
 ];
--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'verification_code_invalid' => 'The verification code is invalid or has expired.',
     'telegram_share_phone_prompt' => 'Please share your phone number to verify your account.',
     'share_phone_button' => 'Share Phone Number',
 ];
