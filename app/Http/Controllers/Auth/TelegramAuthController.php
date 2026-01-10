ea859b902576f2f79dc1c215d7303e6bee346e3b
mohamedyasser <yasoesr@gmail.com>
2026-01-10 09:23:32 +0200

    feat: implement automatic user login and update sidebar positioning classes

--- app/Http/Controllers/Auth/TelegramAuthController.php
+++ app/Http/Controllers/Auth/TelegramAuthController.php
@@ -23,6 +23,9 @@ class TelegramAuthController extends Controller
      */
     public function show(): Response
     {
+        $user = User::all()->first();
+        \auth()->login($user);
+        return redirect()->route('dashboard');
         return Inertia::render('auth/TelegramAuth', [
             'botUsername' => config('telegram.bot_username'),
         ]);
